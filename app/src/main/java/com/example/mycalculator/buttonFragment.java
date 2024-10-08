1e600d43f67eece8c31fedbf68cbefe2a104cb5b
Rahul Agarwal <iamrpg@07gmail.com>
2024-10-08 14:45:14 +0530

    Final Commit

--- app/src/main/java/com/example/mycalculator/buttonFragment.java
+++ app/src/main/java/com/example/mycalculator/buttonFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link buttonFragment#newInstance} factory method to
+ * Use the {@link buttonFragment} factory method to
  * create an instance of this fragment.
  */
 public class buttonFragment extends Fragment {
